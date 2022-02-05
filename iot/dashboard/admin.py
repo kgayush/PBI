806d2441b52c36692595a46534175d8bf9e32e1e
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-02-05 22:06:32 +0530

    Updated dashboard.html

--- iot/dashboard/admin.py
+++ iot/dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Data
 
 # Register your models here.
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ("id", "floor", "room", "product", "status")
+
+admin.site.register(Data, DataAdmin)
