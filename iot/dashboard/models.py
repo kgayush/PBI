44a5af7b80c2dc84b6ec4ca5e12d8c72dc42c4f3
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-02-05 13:15:29 +0530

    Model created

--- iot/dashboard/models.py
+++ iot/dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Data(models.Model):
+    floor = models.CharField(max_length=64)
+    room = models.IntegerField()
+    product = models.CharField(max_length=64)
+    status = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.floor} {self.room} {self.product} {self.status}"
+
